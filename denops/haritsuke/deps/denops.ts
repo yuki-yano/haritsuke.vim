d116bce77896625a5ec19a6648a8957d1c5721b2
yuki-yano <5423775+yuki-yano@users.noreply.github.com>
2025-11-15 00:04:37 +0000

    chore: update dependencies

--- denops/haritsuke/deps/denops.ts
+++ denops/haritsuke/deps/denops.ts
@@ -1,6 +1,6 @@
 // denops standard library exports
-export type { Denops } from "jsr:@denops/std@7.6.0"
-export * as vars from "jsr:@denops/std@7.6.0/variable"
-export * as autocmd from "jsr:@denops/std@7.6.0/autocmd"
-export * as fn from "jsr:@denops/std@7.6.0/function"
-export * as helpers from "jsr:@denops/std@7.6.0/helper"
+export type { Denops } from "jsr:@denops/std@8.1.1"
+export * as vars from "jsr:@denops/std@8.1.1/variable"
+export * as autocmd from "jsr:@denops/std@8.1.1/autocmd"
+export * as fn from "jsr:@denops/std@8.1.1/function"
+export * as helpers from "jsr:@denops/std@8.1.1/helper"
